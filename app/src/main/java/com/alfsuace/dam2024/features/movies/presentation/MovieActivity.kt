8b8b3486f07f43d26bd9cba81e0d6ccf7683e82f
alfsuace <alfonso.suaace@educa.jcyl.es>
2024-09-20 17:28:14 +0200

    basic flow

--- app/src/main/java/com/alfsuace/dam2024/features/movies/presentation/MovieActivity.kt
+++ app/src/main/java/com/alfsuace/dam2024/features/movies/presentation/MovieActivity.kt
@@ -1,22 +1,18 @@
 package com.alfsuace.dam2024.features.movies.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.alfsuace.dam2024.R
-import com.alfsuace.dam2024.features.movies.data.MovieDataRepository
-import com.alfsuace.dam2024.features.movies.data.remote.MovieMockRemoteDataSource
-import com.alfsuace.dam2024.features.movies.domain.GetMoviesUseCase
-import com.alfsuace.dam2024.features.movies.domain.MovieRepository
 
 class MovieActivity : AppCompatActivity() {
 
-    private val movieFactory : MovieFactory = MovieFactory()
+    private val movieFactory: MovieFactory = MovieFactory()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val viewModel = movieFactory.buildViewModel()
+        val movies = viewModel.viewCreated()
+        Log.d("@dev", movies.toString())
     }
 }
--- app/src/main/java/com/alfsuace/dam2024/features/movies/presentation/MovieViewModel.kt
+++ app/src/main/java/com/alfsuace/dam2024/features/movies/presentation/MovieViewModel.kt
@@ -2,9 +2,12 @@ package com.alfsuace.dam2024.features.movies.presentation
 
 import androidx.lifecycle.ViewModel
 import com.alfsuace.dam2024.features.movies.domain.GetMoviesUseCase
+import com.alfsuace.dam2024.features.movies.domain.Movie
 
 class MovieViewModel(private val getMoviesUseCase: GetMoviesUseCase):ViewModel() {
 
-
+fun viewCreated(): List<Movie> {
+    return getMoviesUseCase.invoke()
+}
 
 }
